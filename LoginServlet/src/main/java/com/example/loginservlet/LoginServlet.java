4272cf5714d2a9f89e5b3e2ca1d77a0ebb2eadfa
baekseungyeop <tmdduq5498@gmail.com>
2021-08-17 14:51:19 +0900

    메이븐 pom.xml 라이브러리 추가, 라이브러리 기능 이용 가능 여부 테스트

---
+++ LoginServlet/src/main/java/com/example/loginservlet/LoginServlet.java
@@ -0,0 +1,41 @@
+package com.example.loginservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class  LoginServlet extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        // http:localhost:8080/LoginServlet_war_exploded/login?id=hhhh&passwd=ffffff -- 웹부라우저(client)에서 요청 (GET방식, HTTPMETHOD)
+        // 1. client의 요청을 받아 오는 부분
+        String id = request.getParameter("id");
+        String passwd = request.getParameter("passwd");
+        // 연습 : 이름(name), 나이(age) 추가
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");
+
+        // 2. 어떤 방식으로 응답해 줄 것인가?
+        response.setContentType("text/html;charset=UTF-8");
+
+        // 3. 응답할 내용 데이터를 어떻게 보여줄 것인가?
+        PrintWriter out = response.getWriter();
+        out.println("아이디 = " + id + "<br>");
+        out.println("비밀번호 = " + passwd + "<br>");
+        out.println("이름 = " + name + "<br>");
+        out.println("나이 = " + age + "<br>" );
+    }
+}
+
+
